c27f1abd7efd7e475200a691d64a80c504fe7f60
Drew Miller <drew@onpatrol.co>
2015-05-27 23:59:52 +0100

    Trying to implement more tests

--- test/statusTest.js
+++ test/statusTest.js
@@ -5,9 +5,25 @@ var request = require('supertest');
 
 var destinyStatus = require('../lib/destiny-status');
 
-var app = destinyStatus.setup({});
 
 describe('GET /status', function () {
+    var dummyDb = {};
+    dummyDb.collection = function () { return { find: function () {} }; };
+
+    var app = destinyStatus.setup(dummyDb);
+
+    it('responds with JSON', function (done) {
+        request(app)
+            .get('/status/psn/psnplayer')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+
+describe('GET /status', function () {
+    var app = destinyStatus.setup({});
+
     it('responds with JSON', function (done) {
         request(app)
             .get('/status')
