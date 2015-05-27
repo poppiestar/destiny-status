b780566b2b10512cdd757c24877f2c1bed300c73
Drew Miller <drew@wwdrew.com>
2015-05-27 14:40:49 +0100

    Moved core functionality into a separate module

--- index.js
+++ index.js
@@ -3,46 +3,21 @@ var express = require('express');
 var logger = require('winston');
 
 var app = express();
-var bodyParser = require('body-parser');
 
 var mongoClient = require('mongodb').MongoClient;
-var mongoInstance = 'mongodb://localhost:27017/destiny-status';
 
-app.use(bodyParser.urlencoded({ extended: false, parameterLimit: 2 }));
+var destinyStatus = require('./lib/destiny-status');
+
 app.set('port', (process.env.PORT || 4040));
+app.set('mongoUrl', (process.env.PORT || 'mongodb://localhost:27017/destiny-status'));
 
-mongoClient.connect(mongoInstance, function (err, db) {
+mongoClient.connect(app.get('mongoUrl'), function (err, db) {
     if (err) {
         logger.error(err);
         return;
-    } else {
-        logger.info('Connected to MongoDB');
     }
 
-    app.get('/status', function (req, res) {
-        if (req.query.username && req.query.system) {
-            var collection = db.collection('statuses');
-
-            collection.find({}).toArray(function (err, data) {
-                if (err) {
-                    logger.error('Error retrieving statuses from database');
-                    return;
-                }
-
-                res.json({ status: true, username: req.query.username, system: req.query.system, data: data });
-            });
-        } else {
-            res.status(400).json({ error: 'Username or system type not provided' });
-        }
-    });
-
-    app.post('/login', function (req, res) {
-        if (req.body.username && req.body.password) {
-            res.status(400).json({ username: req.body.username, password: req.body.password });
-        } else {
-            res.status(400).json({ error: 'Username or password not provided' });
-        }
-    });
+    app.use(destinyStatus.setup(db));
 
     // connect to bungie.net and get the current list of activities
     // fail if there's an error, otherwise start the server
