72ce013a793628ad4dcf0504c0ce76c384db6beb
Drew Miller <drew@onpatrol.co>
2015-05-28 00:19:52 +0100

    Improving tests and subsequent code

--- test/statusTest.js
+++ test/statusTest.js
@@ -5,31 +5,37 @@ var request = require('supertest');
 
 var destinyStatus = require('../lib/destiny-status');
 
+var app = destinyStatus.setup({});
 
-describe('GET /status', function () {
-    var dummyDb = {};
-    dummyDb.collection = function () { return { find: function () {} }; };
-
-    var app = destinyStatus.setup(dummyDb);
-
-    it('responds with JSON', function (done) {
-        request(app)
-            .get('/status/psn/psnplayer')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
+describe('getting player statuses', function () {
+    describe('player who exists', function () {
+        it('responds with player status', function (done) {
+            request(app)
+                .get('/status/psn/psnplayer')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
     });
-});
 
-describe('GET /status', function () {
-    var app = destinyStatus.setup({});
+    describe('player who doesn\'t exist', function () {
+        it('responds with a player not found error', function (done) {
+            request(app)
+                .get('/status/psn/mystery')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(400, done);
+        });
+    });
 
-    it('responds with JSON', function (done) {
-        request(app)
-            .get('/status')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(400, done);
+    describe('missing parameters', function () {
+        it('responds with an invalid request error', function (done) {
+            request(app)
+                .get('/status')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(400, done);
+        });
     });
 });
 
