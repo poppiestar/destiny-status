4d6ab627c5bc32a8fb1882c6a5e13f7f3806e5e2
Drew Miller <drew@onpatrol.co>
2015-06-02 09:17:07 +0100

    Rendering an example status display page

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ var app = express();
 var mongoose = require('mongoose');
 
 var destinyStatus = require('./lib/destiny-status');
+var activities = require('./lib/activities');
 var secrets = require('./config/secrets');
 
 app.set('port', (process.env.PORT || 4040));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-mongoose.connect(secrets.db);
-
-var 
 mongoose.connect(secrets.db, function (err, db) {
     if (err) {
         logger.error(err);
@@ -21,6 +21,16 @@ mongoose.connect(secrets.db, function (err, db) {
     }
 
     app.use(destinyStatus.setup(db));
+
+    app.get('/status/:system?/:username?/edit', function (req, res) {
+        if (req.params.username && req.params.system) {
+            var username = req.params.username;
+            var system = req.params.system;
+
+            var playerStatus = destinyStatus.getPlayerStatus(username, system);
+            res.render('status/edit', { activities: activities, status: playerStatus });
+        }
+    });
 
     // connect to bungie.net and get the current list of activities
     // fail if there's an error, otherwise start the server
